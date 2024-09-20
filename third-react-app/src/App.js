5b20cf262a2b0e02614f3eb1570b645a40aa442d
SAITEJA9955 <sivasaitejat1995@gmail.com>
2024-09-20 13:13:36 +0530

    Add files via upload

---
+++ third-react-app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='heading1'>jsxml</h1>
+      <img src='https://www.notsoporangi.com/upload/media/entries/2018-06/11/746-0-12fad64ea66aa5787fbfcce858e32cb7.jpg'width='300px' ></img>
+      <p>Uppalapati Venkata Suryanarayana Prabhas Raju ([pɾabʱaːs] born 23 October 1979), known mononymously as Prabhas, is an Indian actor who predominantly works in Telugu cinema.[4] He is one of the highest-paid actors in Indian cinema and has been featured in Forbes India's Celebrity 100 list since 2015.[9] Referred to in the media as the "Rebel Star", he has appeared in over 20 films, and has received seven Filmfare Awards nominations, a Nandi Award, and a SIIMA Award.[10][11]
+
+Prabhas made his acting debut with the drama Eeswar (2002), and later attained his breakthrough with the action romance Varsham (2004). He went on to star in other commercially successful films such as Chatrapathi (2005), Bujjigadu (2008), Billa (2009), Darling (2010), Mr. Perfect (2011), and Mirchi (2013), winning the Nandi Award for Best Actor for his performance in the lattermost.[12][13] He went on to play a dual role in the epic action duology Baahubali: The Beginning (2015) and Baahubali 2: The Conclusion (2017), with the latter emerging as the highest-grossing Indian film at that point, establishing Prabhas as the first pan-Indian star.[14][15] After a brief period of decline, he made a career comeback with the action drama Salaar: Part 1 – Ceasefire (2023) and the science fiction film Kalki 2898 AD (2024).[16][17]
+
+Prabhas is the only Indian actor to have six films with a worldwide gross opening of over ₹100 crore. He is also the only South Indian actor with six films that have crossed the ₹100 crore nett mark in the Hindi market. Additionally, Prabhas is the first South Indian actor to receive a wax sculpture at a Madame Tussaud's museum.</p>
+     
+    
+      <form>
+      <div>
+        <label htmlfor='e-mail'>e-mail</label>
+        <input id='e-mail'></input>
+      </div>
+  
+    </form>
+
+
+
+    </div>
+  
+  );
+}
+
+export default App;
